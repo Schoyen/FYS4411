d16684d7b22994a151b7951c059284ef9f08d0bb
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2018-01-29 00:39:01 +0100

    Create a new step

--- project_1/vmc_hybrid/src/metropolis_sampling.c
+++ project_1/vmc_hybrid/src/metropolis_sampling.c
@@ -1,7 +1,9 @@
-#include <stdlib.h>
-
 #include "metropolis_sampling.h"
 
 double perform_metropolis_step(double step_length)
+{
+    double step = step_length*(2.0*RANDOM_UNIFORM_DOUBLE - 1.0);
+    return step;
+}
 {
 }
