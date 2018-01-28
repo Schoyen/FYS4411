15c3614278ca5f4d3713daf0e66ddcf2d8286ce1
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2018-01-29 00:38:18 +0100

    Create macro for generating random uniform doubles

--- project_1/vmc_hybrid/include/metropolis_sampling.h
+++ project_1/vmc_hybrid/include/metropolis_sampling.h
@@ -1,6 +1,13 @@
 #ifndef METROPOLIS_SAMPLING_H
 #define METROPOLIS_SAMPLING_H
 
+#include <stdlib.h>
+#include <limits.h>
+
+#define RANDOM_UNIFORM_DOUBLE \
+    (((((unsigned long) arc4random() << 32) | arc4random()) \
+     / ((double) UINT64_MAX)))
+
 double perform_metropolis_step(double step_length);
 void metropolis_sampling();
 
