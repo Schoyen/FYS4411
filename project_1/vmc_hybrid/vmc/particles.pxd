185c4f7ae8d83780ce528d79dca8b881ae4e3ad3
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2018-01-28 22:00:40 +0100

    Fetch dimensionality from enum in particles.h

--- project_1/vmc_hybrid/vmc/particles.pxd
+++ project_1/vmc_hybrid/vmc/particles.pxd
@@ -1,6 +1,9 @@
 cdef extern from "particles.h":
+    cdef enum:
+        DIMENSIONALITY = 3
+
     cdef struct particle:
-        double *position
+        double position[DIMENSIONALITY]
 
     cdef struct particles:
         particle *particles
--- project_1/vmc_hybrid/vmc/particles.pyx
+++ project_1/vmc_hybrid/vmc/particles.pyx
@@ -3,5 +3,7 @@ from libc.stdlib cimport malloc, free
 cdef class Particles:
     cdef particles *m_particles
 
-    def __cinit__(self):
-        m_particles = NULL
+    def __cinit__(self, int num_walkers):
+        m_particles = <particles *> malloc(sizeof(particles))
+        m_particles.particles = \
+            <particle *> malloc(sizeof(particle)*num_walkers)
