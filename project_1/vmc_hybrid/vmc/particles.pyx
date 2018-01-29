a4485f3848e195f6cdca330de9b2d1b85b0a3750
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2018-01-29 11:53:46 +0100

    Add functions for initializing the particles

--- project_1/vmc_hybrid/vmc/particles.pyx
+++ project_1/vmc_hybrid/vmc/particles.pyx
@@ -1,5 +1,8 @@
 from libc.stdlib cimport malloc, free
 
+import numpy as np
+cimport numpy as np
+
 cdef class Particles:
     cdef particles m_particles
 
@@ -10,6 +13,18 @@ cdef class Particles:
 
         if not self.m_particles.particles:
             raise Exception("particles-array was not allocated")
+
+    def initialize_particles(self, double spread):
+        cdef unsigned int i
+
+        for i in range(self.m_particles.num_particles):
+            self.initialize_particles(self.m_particles.particles[i], spread)
+
+    cdef inline intialize_particle(self, particle *particle, double spread):
+        cdef int i
+
+        for i in range(DIMENSIONALITY):
+            particle.position[i] = spread*(2.0*np.random.random() - 1.0)
 
     def __dealloc__(self):
         free(self.m_particles.particles)
