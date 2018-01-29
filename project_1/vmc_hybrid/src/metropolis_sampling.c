da862f377db701d7b185f7eec459bd7bfc78283d
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2018-01-29 11:04:10 +0100

    Add Metropolis test

--- project_1/vmc_hybrid/src/metropolis_sampling.c
+++ project_1/vmc_hybrid/src/metropolis_sampling.c
@@ -1,10 +1,15 @@
 #include "metropolis_sampling.h"
 
-double perform_metropolis_step(particles_t *particles, double step_length)
+double perform_metropolis_step(
+        particles_t *particles, double step_length,
+        double (*local_energy)(double position),
+        double (*ratio)(double new_position, double old_position))
 {
     unsigned int particle_index, dimension_index;
-    double step, proposed_position;
+    double step, old_position, new_position, weight, delta_energy;
     particle_t *particle;
+
+    delta_energy = 0;
 
     /* Draw a random particle */
     particle_index = arc4random_uniform(particles->num_particles);
@@ -16,10 +21,25 @@ double perform_metropolis_step(particles_t *particles, double step_length)
 
     /* Do a step from [-step_length, step_length) */
     step = step_length*(2.0*RANDOM_UNIFORM_DOUBLE - 1.0);
+    /* Get the previous position */
+    old_position = particle->position[dimension_index];
     /* Propose a new position */
-    proposed_position = particle->position[dimension_index] + step;
+    new_position = old_position + step;
 
-    return step;
+    /* Compute the ratio between the new and the old position */
+    weight = (*ratio)(new_position, old_position);
+
+    /* See if the new position should be accepted */
+    if (weight >= RANDOM_UNIFORM_DOUBLE) {
+        /* Compute the change in energy for the new position */
+        delta_energy = (*local_energy)(new_position) \
+            - (*local_energy)(old_position);
+        /* Update the position of the particle */
+        particle->position[dimension_index] = new_position;
+    }
+
+    /* Return the change in energy */
+    return delta_energy;
 }
 
 void metropolis_sampling(void)
