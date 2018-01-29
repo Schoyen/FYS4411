288347189668dbabe502e42248a42920bb82fc7f
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2018-01-29 20:02:15 +0100

    Add ratio function

--- project_1/vmc_hybrid/src/one_dimensional_ho.c
+++ project_1/vmc_hybrid/src/one_dimensional_ho.c
@@ -12,7 +12,23 @@ double ratio(
         parameters_t *parameters, double new_position[DIMENSIONALITY],
         double old_position[DIMENSIONALITY])
 {
-    return 0.0;
+    double new_position_squared, old_position_squared, alpha;
+    unsigned int i;
+
+    /* Calculate the positions squared */
+    new_position_squared = 0.0;
+    old_position_squared = 0.0;
+
+    for (i = 0; i < DIMENSIONALITY; i++) {
+        new_position_squared += new_position[i]*new_position[i];
+        old_position_squared += old_position[i]*old_position[i];
+    }
+
+    /* Fetch the variational parameter */
+    alpha = parameters->parameters[0];
+
+    /* Return the ratio */
+    return exp(-alpha*(new_position_squared - old_position_squared));
 }
 
 double wavefunction(
