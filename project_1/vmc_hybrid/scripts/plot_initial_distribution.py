4ba573c51da5b798e0eb779c5412eb37cc879be3
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2018-01-29 12:33:58 +0100

    Plot initial distribution of walkers

--- project_1/vmc_hybrid/scripts/plot_initial_distribution.py
+++ project_1/vmc_hybrid/scripts/plot_initial_distribution.py
@@ -1,1 +1,12 @@
 from particles import Particles
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+system = Particles(50)
+system.initialize_particles(3.0)
+
+particle_array = system.get_particles()
+
+ax = plt.figure().add_subplot(111, projection="3d")
+ax.scatter(particle_array[:, 0], particle_array[:, 1], particle_array[:, 2])
+plt.show()
