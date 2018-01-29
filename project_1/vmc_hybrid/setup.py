2583ffba0ef614b5c11f9855ff7fcba0426f59d8
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2018-01-29 20:33:49 +0100

    Split into two extensions

--- project_1/vmc_hybrid/setup.py
+++ project_1/vmc_hybrid/setup.py
@@ -4,11 +4,15 @@ from Cython.Build import cythonize
 
 import numpy as np
 
-source_files = [
+particles_source_files = [
     "vmc/particles.pyx",
     "src/metropolis_sampling.c",
     #"src/bosonic_hard_sphere.c",
     "src/one_dimensional_ho.c"
+]
+
+wavefunction_source_files = [
+    "vmc/wavefunction.pyx"
 ]
 
 
@@ -23,7 +27,14 @@ libraries = []
 extensions = [
     Extension(
         name="particles",
-        sources=source_files,
+        sources=particles_source_files,
+        language="c",
+        include_dirs=include_dirs,
+        libraries=libraries
+    ),
+    Extension(
+        name="wavefunction",
+        sources=wavefunction_source_files,
         language="c",
         include_dirs=include_dirs,
         libraries=libraries
