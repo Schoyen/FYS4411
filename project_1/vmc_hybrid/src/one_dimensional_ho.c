4d0fc93e4afdc465a7bc05e4f9e376503382515e
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2018-01-29 18:37:18 +0100

    Add function skeleton

--- project_1/vmc_hybrid/src/one_dimensional_ho.c
+++ project_1/vmc_hybrid/src/one_dimensional_ho.c
@@ -1,1 +1,20 @@
 #include "wavefunction.h"
+
+double local_energy(
+        parameters_t *parameters, double position[DIMENSIONALITY])
+{
+    return 0.0;
+}
+
+double ratio(
+        parameters_t *parameters, double new_position[DIMENSIONALITY],
+        double old_position[DIMENSIONALITY])
+{
+    return 0.0;
+}
+
+double wavefunction(
+        parameters_t *parameters, double position[DIMENSIONALITY])
+{
+    return 0.0;
+}
