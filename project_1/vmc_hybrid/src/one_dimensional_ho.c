3dfc9700fd4748f2e79092c754781a5036b3ccae
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2018-01-29 19:58:09 +0100

    Add function returning the trial wavefunction

--- project_1/vmc_hybrid/src/one_dimensional_ho.c
+++ project_1/vmc_hybrid/src/one_dimensional_ho.c
@@ -1,3 +1,5 @@
+#include <cmath.h>
+
 #include "wavefunction.h"
 
 double local_energy(
@@ -16,5 +18,19 @@ double ratio(
 double wavefunction(
         parameters_t *parameters, double position[DIMENSIONALITY])
 {
-    return 0.0;
+    double position_squared, alpha;
+    unsigned int i;
+
+    /* Calculate the radius/position squared */
+    position_squared = 0.0;
+
+    for (i = 0; i < DIMENSIONALITY; i++) {
+        position_squared += position[i]*position[i];
+    }
+
+    /* Fetch the variational parameter */
+    alpha = parameters->parameters[0];
+
+    /* Return the trial wavefunction */
+    return exp(-alpha*position_squared);
 }
