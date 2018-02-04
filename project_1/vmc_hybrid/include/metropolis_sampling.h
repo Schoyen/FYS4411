9e8390bbc923d5c33f276fdc10928422019fabc5
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2018-02-04 18:13:39 +0100

    Use wavefunction as parameters to metropolis

--- project_1/vmc_hybrid/include/metropolis_sampling.h
+++ project_1/vmc_hybrid/include/metropolis_sampling.h
@@ -4,8 +4,6 @@
 #include <stdlib.h>
 #include <limits.h>
 
-#include "wavefunction_config.h"
-#include "particles.h"
 #include "wavefunction.h"
 
 /* Create a macro for creating a uniform double in the interval [0, 1) */
@@ -14,14 +12,10 @@
      / ((double) UINT64_MAX)))
 
 double perform_metropolis_step(
-        particles_t *particles,
-        parameters_t *parameters,
-        double step_length);
+        wavefunction_t *wavefunction, double step_length);
 
 double metropolis_sampling(
-        particles_t *particles,
-        parameters_t *parameters,
-        double step_length,
+        wavefunction_t *wavefunction, double step_length,
         unsigned int num_samples);
 
 #endif
