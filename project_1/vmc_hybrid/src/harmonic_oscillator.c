a764bf008af785bd8df16cda40ed48ceb1df9ccd
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2018-02-05 14:42:07 +0100

    Add correct local energy expression

--- project_1/vmc_hybrid/src/harmonic_oscillator.c
+++ project_1/vmc_hybrid/src/harmonic_oscillator.c
@@ -4,27 +4,29 @@
 #include "wavefunction.h"
 
 
-double local_energy(
-        wavefunction_t *wavefunction)
+double local_energy(wavefunction_t *wavefunction)
 {
-    double position_squared_sum, alpha;
+    double alpha, position_squared_sum;
     unsigned int i, j;
 
-    /* Initialize position squared sum */
+    /* Fetch the variational parameter */
+    alpha = wavefunction->parameters[0];
+
+    /* Initialize the sum */
     position_squared_sum = 0.0;
 
-    /* Calculate the position squared sum */
+    // TODO: Store the parameters of the Hamiltonian, e.g., omega, mass
+
+    /* Add all positions */
     for (i = 0; i < wavefunction->num_particles; i++) {
         for (j = 0; j < wavefunction->dimensionality; j++) {
             position_squared_sum += SQUARE(wavefunction->particles[i][j]);
         }
     }
 
-    /* Fetch the variational parameter */
-    alpha = wavefunction->parameters[0];
-
     /* Return the local energy */
-    return alpha + position_squared_sum*(0.5 - 2*alpha*alpha);
+    return (-2*SQUARE(alpha) + 0.5)*position_squared_sum
+        + wavefunction->dimensionality*wavefunction->num_particles*alpha;
 }
 
 double ratio(
