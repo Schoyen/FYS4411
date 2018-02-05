9dea900831c3ccfb77cedc2fb3c77f846432e080
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2018-02-05 10:56:13 +0100

    Add allocation and freeing of wavefunction

--- project_1/vmc_hybrid/src/wavefunction.c
+++ project_1/vmc_hybrid/src/wavefunction.c
@@ -1,3 +1,24 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 #include "wavefunction.h"
+
+
+void allocate_variational_parameters(wavefunction_t *wavefunction)
+{
+    /* Allocate memory for parameters */
+    wavefunction->parameters =
+        (double *) calloc(wavefunction->num_parameters, sizeof(double));
+
+    /* Check if the allocation succeeded */
+    if (!wavefunction->parameters) {
+        fprintf(stderr, "Allocation of parameters failed\n");
+        exit(EXIT_FAILURE);
+    }
+}
+
+void free_variational_parameters(wavefunction_t *wavefunction)
+{
+    /* Free parameter array memory */
+    free(wavefunction->parameters);
+}
