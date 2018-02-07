30088ac146f7acbe46240f71597801170051cf6d
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2018-02-07 23:50:36 +0100

    Remove source code for Hamiltonian
    	This will be dependent on the system and should only be
    	implemented in its own respective source code.

--- project_1/vmc_hybrid/include/hamiltonian.h
+++ project_1/vmc_hybrid/include/hamiltonian.h
@@ -1,4 +1,8 @@
 #ifndef HAMILTONIAN_H
 #define HAMILTONIAN_H
 
+typedef struct hamiltonian hamiltonian_t;
+
+double local_energy(wavefunction_t *wavefunction, hamiltonian_t *hamiltonian);
+
 #endif
--- project_1/vmc_hybrid/src/hamiltonian.c
+++
@@ -1,1 +0,0 @@
-#include "hamiltonian.h"
