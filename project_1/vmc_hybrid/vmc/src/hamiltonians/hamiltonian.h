f3cd0a250baf1e8d8cdfe0dab26a7d6a589be854
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2018-02-08 00:21:42 +0100

    Let hamiltonian include the wavefunction header

--- project_1/vmc_hybrid/vmc/src/hamiltonians/hamiltonian.h
+++ project_1/vmc_hybrid/vmc/src/hamiltonians/hamiltonian.h
@@ -1,6 +1,8 @@
 #ifndef HAMILTONIAN_H
 #define HAMILTONIAN_H
 
+#include "wavefunction.h"
+
 typedef struct hamiltonian hamiltonian_t;
 
 double local_energy(wavefunction_t *wavefunction, hamiltonian_t *hamiltonian);
