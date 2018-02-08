ecab50ac685e6094b1e1ed778d5b92e9bebc031d
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2018-02-08 20:07:39 +0100

    Add super-class for the Monte Carlo methods
    	This class will allow for reuse of the PRNG's.

---
+++ project_1/vmc_hybrid/vmc/src/solvers/monte_carlo_method.cpp
@@ -0,0 +1,13 @@
+#include <random>
+
+#include "monte_carlo_method.h"
+
+MonteCarloMethod::MonteCarloMethod(unsigned int num_particles)
+{
+    std::random_device rd;
+
+    m_engine(rd);
+
+    m_random_particle = std::uniform_int_distribution<>(0, num_particles - 1);
+    m_random_step = std::uniform_real_distribution<double>(0.0, 1.0);
+}
---
+++ project_1/vmc_hybrid/vmc/src/solvers/monte_carlo_method.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <random>
+
+class MonteCarloMethod
+{
+    protected:
+        std::mt19937 m_engine;
+        std::uniform_int_distribution<> m_random_particle;
+        std::uniform_real_distribution<double> m_random_step;
+
+    public:
+        MonteCarloMethod(unsigned int num_particles);
+}
