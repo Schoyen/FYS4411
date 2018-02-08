82ffec9182486859af53281ff8113f8d4f5d9b38
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2018-02-08 17:26:00 +0100

    Define the HO constructor and local energy

--- project_1/vmc_hybrid/vmc/src/hamiltonians/harmonic_oscillator.c
+++ project_1/vmc_hybrid/vmc/src/hamiltonians/harmonic_oscillator.c
@@ -1,24 +1,28 @@
+#include <cassert>
+
 #include "hamiltonian.h"
+#include "harmonic_oscillator.h"
 #include "wavefunction.h"
-#include "vmc_macros.h"
+#include "math_macros.h"
 
-double local_energy(wavefunction_t *wavefunction, hamiltonian_t *hamiltonian)
+HarmonicOscillator::HarmonicOscillator(double mass, double omega)
 {
-    double alpha, position_squared_sum, kinetic_energy, potential_energy;
-    unsigned int i, j;
+    assert(mass > 0);
+    assert(omega > 0);
 
-    /* Fetch the variational parameter */
-    alpha = wavefunction->parameters[0];
+    m_mass = mass;
+    m_omega = omega;
+}
 
-    /* Initialize the sum */
-    position_squared_sum = 0.0;
+double HarmonicOscillator::compute_local_energy(Wavefunction *wavefunction)
+{
+    double kinetic_energy, potential_energy;
 
-    /* Add all positions */
-    for (i = 0; i < wavefunction->num_particles; i++) {
-        for (j = 0; j < wavefunction->dimensionality; j++) {
-            position_squared_sum += SQUARE(wavefunction->particles[i][j]);
-        }
-    }
+    kinetic_energy = SQUARE(HBAR)*wavefunction->compute_laplacian()/(2*m_mass);
+    potential_energy = compute_potential_energy(wavefunction);
+
+    return kinetic_energy + potential_energy;
+}
 
     kinetic_energy = -SQUARE(hamiltonian->hbar)/(2.0*hamiltonian->mass) \
         * (-2*alpha*wavefunction->dimensionality*wavefunction->num_particles \
@@ -31,3 +35,31 @@ double local_energy(wavefunction_t *wavefunction, hamiltonian_t *hamiltonian)
     return kinetic_energy + potential_energy;
 }
 
+//double HarmonicOscillator::compute_local_energy(Wavefunction *wavefunction)
+//{
+//    double alpha, position_squared_sum, kinetic_energy, potential_energy;
+//    unsigned int i, j;
+//
+//    /* Fetch the variational parameter */
+//    alpha = wavefunction->parameters[0];
+//
+//    /* Initialize the sum */
+//    position_squared_sum = 0.0;
+//
+//    /* Add all positions */
+//    for (i = 0; i < wavefunction->num_particles; i++) {
+//        for (j = 0; j < wavefunction->dimensionality; j++) {
+//            position_squared_sum += SQUARE(wavefunction->particles[i][j]);
+//        }
+//    }
+//
+//    kinetic_energy = -SQUARE(hamiltonian->hbar)/(2.0*hamiltonian->mass) \
+//        * (-2*alpha*wavefunction->dimensionality*wavefunction->num_particles \
+//            + 4*SQUARE(alpha)*position_squared_sum);
+//
+//    potential_energy = 0.5*hamiltonian->mass*SQUARE(hamiltonian->omega) \
+//        * position_squared_sum;
+//
+//    /* Return the local energy */
+//    return kinetic_energy + potential_energy;
+//}
