0a6c49f9697b4a9a01488511453592deff572121
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2018-02-08 22:08:38 +0100

    Create engine in class declaration

--- project_1/vmc_hybrid/vmc/src/solvers/monte_carlo_method.h
+++ project_1/vmc_hybrid/vmc/src/solvers/monte_carlo_method.h
@@ -5,10 +5,10 @@
 class MonteCarloMethod
 {
     protected:
-        std::mt19937 m_engine;
+        std::mt19937 m_engine(std::random_device rd);
         std::uniform_int_distribution<> m_random_particle;
         std::uniform_real_distribution<double> m_random_step;
 
     public:
         MonteCarloMethod(unsigned int num_particles);
-}
+};
