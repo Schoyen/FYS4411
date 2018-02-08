8fcbd7fa7c1ecab148580874fb1484cbd7470d75
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2018-02-08 18:34:28 +0100

    Add Wavefunction class

--- project_1/vmc_hybrid/vmc/src/wavefunctions/wavefunction.h
+++ project_1/vmc_hybrid/vmc/src/wavefunctions/wavefunction.h
@@ -1,27 +1,42 @@
-#ifndef WAVEFUNCTION_H
-#define WAVEFUNCTION_H
+#pragma once
 
-#include "vmc_macros.h"
+class Wavefunction
+{
+    protected:
+        unsigned int m_num_particles;
+        unsigned int m_dimensions;
+        unsigned int m_num_parameters;
 
-typedef struct wavefunction {
-    unsigned int num_particles;
-    unsigned int dimensionality;
-    unsigned int num_parameters;
+        /* This refers to the last value from the evaluation function */
+        bool m_valid_last_value;
+        double m_last_value;
 
-    double last_value;
+        /* This allows us to compute the position squared once per change of
+         * particle movement */
+        bool m_valid_position_squared_sum;
+        double m_last_position_squared_sum;
 
-    double *parameters;
-    double **particles;
-} wavefunction_t;
+        double *m_parameters;
+
+        /* We use single pointer in order to allow for allocation from NumPy.
+         * This array will thus be indexed as:
+         *
+         *      m_particles[j + i*m_dimensions] = m_particles[i][j],
+         *
+         * where i is the particle index and j is the position index. */
+        double *m_particles;
+
+    public:
+        Wavefunction(
+                unsigned int num_particles,
+                unsigned int dimensions,
+                unsigned int num_parameters,
+                double *parameters,
+                double *particles);
 
 
+        double compute_position_squared_sum();
 
-void allocate_variational_parameters(wavefunction_t *wavefunction);
-void free_variational_parameters(wavefunction_t *wavefunction);
-
-void allocate_particles(wavefunction_t *wavefunction);
-void free_particles(wavefunction_t *wavefunction);
-
-double evaluate_wavefunction(wavefunction_t *wavefunction);
-
-#endif
+        virtual double evaluate() = 0;
+        virtual double compute_laplacian() = 0;
+};
