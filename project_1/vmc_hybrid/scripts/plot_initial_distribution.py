45deeea0be5dfda8f5e37719040c286119de68eb
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2018-02-10 22:59:52 +0100

    Update dev-script
    	This is not working as the build fails due to some library
    	mangling.

--- project_1/vmc_hybrid/scripts/plot_initial_distribution.py
+++ project_1/vmc_hybrid/scripts/plot_initial_distribution.py
@@ -1,5 +1,4 @@
-from vmc.metropolis_sampling import Wavefunction, perform_varying_metropolis, \
-        normalize_energies
+from vmc.interface import PySimpleGaussian
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
@@ -11,28 +10,7 @@ spread = 1.0
 step_length = 0.5
 num_samples = 100000
 
-parameters = np.array([[alpha] for alpha in np.linspace(0.1, 2, 101)])
-
-wavefunction = Wavefunction(num_particles, dimensionality, num_parameters)
-wavefunction.initialize_wavefunction(spread)
-
-particle_array = wavefunction.get_particles()
-
-
-# One dimensional plotting for DIMENSIONALITY = 1
-#plt.scatter(np.linspace(0, 50, len(particle_array)), particle_array)
-#plt.show()
-
-# Three dimensional plotting for DIMENSIONALITY = 3
-ax = plt.figure().add_subplot(111, projection="3d")
-ax.scatter(particle_array[:, 0], particle_array[:, 1], particle_array[:, 2])
-plt.show()
-
-
-energies = perform_varying_metropolis(
-        wavefunction, step_length, num_samples, parameters)
-
-energies = normalize_energies(energies, num_samples, num_particles)
+wavefunction = PySimpleGaussian(num_particles, num_dimensions, num_parameters)
 
 particle_array = wavefunction.get_particles()
 ax = plt.figure().add_subplot(111, projection="3d")
