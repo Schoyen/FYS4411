b0a8750d351342cfa4d3be8f0b9ea54f4d23b933
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2018-02-11 19:47:58 +0100

    Start testing of Wavefunction classes

--- project_1/vmc_hybrid/tests/test_wavefunction.py
+++ project_1/vmc_hybrid/tests/test_wavefunction.py
@@ -1,5 +1,20 @@
 from vmc.interface import PySimpleGaussian
+import numpy as np
 
 
 def test_wavefunction():
-    wavefunction = PySimpleGaussian(1, 2, 3)
+    spread = 2.0
+    num_particles = 100
+    num_parameters = 1
+    num_dimensions = 3
+
+    parameters = np.array([0.5])
+
+    wavefunction = PySimpleGaussian(
+            num_particles, num_dimensions, num_parameters, spread=spread)
+
+    particles = wavefunction.get_particles()
+    assert ((particles >= -spread) & (particles <= spread)).all()
+
+    wavefunction.set_parameters(parameters)
+    assert np.allclose(parameters, wavefunction.get_parameters())
