07c0d543eee2daab1a74fe47aa8ee819fcea7816
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2018-02-11 10:25:08 +0100

    Use glob to fetch source code

--- project_1/vmc_hybrid/setup.py
+++ project_1/vmc_hybrid/setup.py
@@ -4,6 +4,7 @@ from Cython.Build import cythonize
 
 import numpy as np
 import os
+import glob
 import platform
 
 
@@ -15,29 +16,14 @@ if platform.system() == "Darwin":
 base_path = ["vmc"]
 source_path = base_path + ["src"]
 
-config_path = source_path + ["config"]
-wavefunctions_path = source_path + ["wavefunctions"]
-hamiltonians_path = source_path + ["hamiltonians"]
-math_path = source_path + ["math"]
-solvers_path = source_path + ["solvers"]
-
-
 source_files = [
         os.path.join(*base_path, "interface.pyx"),
-        os.path.join(*wavefunctions_path, "wavefunction.cpp"),
-        os.path.join(*wavefunctions_path, "simple_gaussian.cpp"),
-        os.path.join(*hamiltonians_path, "harmonic_oscillator.cpp"),
-        os.path.join(*solvers_path, "monte_carlo_method.cpp"),
-        os.path.join(*solvers_path, "metropolis_algorithm.cpp")
+        *glob.glob(os.path.join(*source_path, "*", "*.cpp"))
 ]
 
 
 include_dirs = [
-        os.path.join(*config_path),
-        os.path.join(*wavefunctions_path),
-        os.path.join(*hamiltonians_path),
-        os.path.join(*math_path),
-        os.path.join(*solvers_path),
+        *glob.glob(os.path.join(*source_path, "*")),
         np.get_include()
 ]
 
