63fb2eaeb314fc8aef96c825e8b9bfeda6699728
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2018-02-11 20:53:18 +0100

    Add test for the Hamiltonian

--- project_1/vmc_hybrid/test/test_interface.py
+++ project_1/vmc_hybrid/test/test_interface.py
@@ -1,12 +1,34 @@
-from vmc.interface import PySimpleGaussian
+from vmc.interface import PySimpleGaussian, PyHarmonicOscillator
 import numpy as np
+import pytest
 
-
-def test_wavefunction():
+@pytest.fixture
+def wavefunction_params():
     spread = 2.0
     num_particles = 100
     num_parameters = 1
     num_dimensions = 3
+
+    return spread, num_particles, num_parameters, num_dimensions
+
+@pytest.fixture()
+def one_simple_gauss():
+    spread = 1.0
+    num_particles = 1
+    num_parameters = 1
+    num_dimensions = 1
+
+    return spread, num_particles, num_parameters, num_dimensions
+
+@pytest.fixture
+def hamiltonian_params():
+    mass = 1
+    omega = 1
+
+    return mass, omega
+
+def test_wavefunction(wavefunction_params):
+    spread, num_particles, num_parameters, num_dimensions = wavefunction_params
 
     parameters = np.array([0.5])
 
@@ -18,3 +40,22 @@ def test_wavefunction():
 
     wavefunction.set_parameters(parameters)
     assert np.allclose(parameters, wavefunction.get_parameters())
+
+
+@pytest.mark.run(after="test_wavefunction")
+def test_harmiltonian(hamiltonian_params, one_simple_gauss):
+    mass, omega = hamiltonian_params
+    spread, num_particles, num_parameters, num_dimensions = one_simple_gauss
+
+    ho = PyHarmonicOscillator(mass, omega)
+    wavefunction = PySimpleGaussian(
+            num_particles, num_dimensions, num_parameters, spread=spread)
+
+    parameters = np.array([0.5])
+    wavefunction.set_parameters(parameters)
+
+    local_energy = ho.compute_local_energy(wavefunction)
+
+    print (local_energy)
+
+    assert False
