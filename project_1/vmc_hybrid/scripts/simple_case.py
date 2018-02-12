3e605eab55b1667c90a5f9b0c5f77d5b6609f6b4
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2018-02-12 15:31:16 +0100

    Run for smaller system

--- project_1/vmc_hybrid/scripts/simple_case.py
+++ project_1/vmc_hybrid/scripts/simple_case.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 import tqdm
 
 
-num_particles = 500#[1, 10, 100, 500]
+num_particles = 50#[1, 10, 100, 500]
 num_dimensions = 3
 num_parameters = 1
 spread = 1.0
@@ -20,9 +20,9 @@ num_samples = 1000000
 mass = 1.0
 omega = 1.0
 
-alpha_min = 0.1
-alpha_max = 2.0
-num_alphas = 50
+alpha_min = 0.2
+alpha_max = 0.7
+num_alphas = 21
 
 alphas = np.linspace(alpha_min, alpha_max, num_alphas).reshape(num_alphas, 1)
 
@@ -33,11 +33,17 @@ hamiltonian = PyHarmonicOscillator(mass, omega)
 method = PyMetropolisAlgorithm(num_particles)
 
 energies = np.zeros(num_alphas)
+local_energies_list = []
 
-for i in tqdm.tqdm(range(num_alphas)):
+for i in range(num_alphas):
     wavefunction.set_parameters(alphas[i])
     energies[i] = method.run(
             wavefunction, hamiltonian, step_length, num_samples)
+
+    print ("alphas = {0}\tenergy = {1}".format(
+        alphas[i], energies[i]/num_samples))
+
+    wavefunction.redistribute()
 
 plt.plot(alphas.ravel(), energies/num_samples)
 plt.show()
