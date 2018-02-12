2f39899cfb2b292b81d04c0fe828002060283b37
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2018-02-12 09:53:58 +0100

    Remove old commented code

--- project_1/vmc_hybrid/vmc/src/hamiltonians/harmonic_oscillator.cpp
+++ project_1/vmc_hybrid/vmc/src/hamiltonians/harmonic_oscillator.cpp
@@ -34,32 +34,3 @@ double HarmonicOscillator::compute_potential_energy(Wavefunction *wavefunction)
 
     return potential_energy;
 }
-
-//double HarmonicOscillator::compute_local_energy(Wavefunction *wavefunction)
-//{
-//    double alpha, position_squared_sum, kinetic_energy, potential_energy;
-//    unsigned int i, j;
-//
-//    /* Fetch the variational parameter */
-//    alpha = wavefunction->parameters[0];
-//
-//    /* Initialize the sum */
-//    position_squared_sum = 0.0;
-//
-//    /* Add all positions */
-//    for (i = 0; i < wavefunction->num_particles; i++) {
-//        for (j = 0; j < wavefunction->dimensionality; j++) {
-//            position_squared_sum += SQUARE(wavefunction->particles[i][j]);
-//        }
-//    }
-//
-//    kinetic_energy = -SQUARE(hamiltonian->hbar)/(2.0*hamiltonian->mass) \
-//        * (-2*alpha*wavefunction->dimensionality*wavefunction->num_particles \
-//            + 4*SQUARE(alpha)*position_squared_sum);
-//
-//    potential_energy = 0.5*hamiltonian->mass*SQUARE(hamiltonian->omega) \
-//        * position_squared_sum;
-//
-//    /* Return the local energy */
-//    return kinetic_energy + potential_energy;
-//}
