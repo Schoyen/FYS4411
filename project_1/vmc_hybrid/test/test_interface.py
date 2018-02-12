1be307bec054bb0885118f4f915bf5f94942ae6d
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2018-02-12 09:53:17 +0100

    Add basic tests for the local energies

--- project_1/vmc_hybrid/test/test_interface.py
+++ project_1/vmc_hybrid/test/test_interface.py
@@ -20,6 +20,24 @@ def one_simple_gauss():
 
     return spread, num_particles, num_parameters, num_dimensions
 
+@pytest.fixture()
+def two_simple_gauss():
+    spread = 1.0
+    num_particles = 1
+    num_parameters = 1
+    num_dimensions = 2
+
+    return spread, num_particles, num_parameters, num_dimensions
+
+@pytest.fixture()
+def three_simple_gauss():
+    spread = 1.0
+    num_particles = 1
+    num_parameters = 1
+    num_dimensions = 3
+
+    return spread, num_particles, num_parameters, num_dimensions
+
 @pytest.fixture
 def hamiltonian_params():
     mass = 1
@@ -43,7 +61,7 @@ def test_wavefunction(wavefunction_params):
 
 
 @pytest.mark.run(after="test_wavefunction")
-def test_harmiltonian(hamiltonian_params, one_simple_gauss):
+def test_one_dimensional_ho(hamiltonian_params, one_simple_gauss):
     mass, omega = hamiltonian_params
     spread, num_particles, num_parameters, num_dimensions = one_simple_gauss
 
@@ -56,5 +74,39 @@ def test_harmiltonian(hamiltonian_params, one_simple_gauss):
 
     local_energy = ho.compute_local_energy(wavefunction)
     expected_value = 0.5
+
+    assert np.allclose(local_energy, expected_value)
+
+@pytest.mark.run(after="test_one_dimensional_ho")
+def test_two_dimensional_ho(hamiltonian_params, two_simple_gauss):
+    mass, omega = hamiltonian_params
+    spread, num_particles, num_parameters, num_dimensions = two_simple_gauss
+
+    ho = PyHarmonicOscillator(mass, omega)
+    wavefunction = PySimpleGaussian(
+            num_particles, num_dimensions, num_parameters, spread=spread)
+
+    parameters = np.array([0.5])
+    wavefunction.set_parameters(parameters)
+
+    local_energy = ho.compute_local_energy(wavefunction)
+    expected_value = 1.0
+
+    assert np.allclose(local_energy, expected_value)
+
+@pytest.mark.run(after="test_two_dimensional_ho")
+def test_three_dimensional_ho(hamiltonian_params, three_simple_gauss):
+    mass, omega = hamiltonian_params
+    spread, num_particles, num_parameters, num_dimensions = three_simple_gauss
+
+    ho = PyHarmonicOscillator(mass, omega)
+    wavefunction = PySimpleGaussian(
+            num_particles, num_dimensions, num_parameters, spread=spread)
+
+    parameters = np.array([0.5])
+    wavefunction.set_parameters(parameters)
+
+    local_energy = ho.compute_local_energy(wavefunction)
+    expected_value = 1.5
 
     assert np.allclose(local_energy, expected_value)
