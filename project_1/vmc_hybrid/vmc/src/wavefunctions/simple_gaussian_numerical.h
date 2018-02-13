ba3b89de284a87ac9753550b13b518c9dbbfc13c
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2018-02-13 14:12:07 +0100

    Add subclass of the simple gaussian
    	This subclass overrides the laplacian function with its
    	numerical approximation.

---
+++ project_1/vmc_hybrid/vmc/src/wavefunctions/simple_gaussian_numerical.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "wavefunction.h"
+#include "simple_gaussian.h"
+
+class SimpleGaussianNumerical : public SimpleGaussian
+{
+    private:
+        double m_h;
+
+    public:
+        SimpleGaussianNumerical(
+                unsigned int num_particles,
+                unsigned int num_dimensions,
+                unsigned int num_parameters,
+                double mass,
+                double omega,
+                double h,
+                double *parameters,
+                double *particles);
+
+        double compute_laplacian();
+};
