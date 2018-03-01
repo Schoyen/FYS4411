eeb28ddaab7860482d582e439494c02214b4bf9d
gregwinther <sebastwi@uio.no>
2018-03-01 11:49:44 +0100

    Everything was different this morning. I was pretty bummed about it. But then I thought that the person that made the change was probably more happy to see it change than I was to accomodate the changes. The net happiness in the world increased. So whatever.

--- project_1/vmc_hybrid/scripts/importance_compare.py
+++ project_1/vmc_hybrid/scripts/importance_compare.py
@@ -1,6 +1,6 @@
 import config
 
-from vmc.interface import PySimpleGaussian, PyHarmonicOscillator, PyMetropolisAlgorithm, PyImportanceMetropolis
+from vmc.interface import PySimpleGaussian, PyHarmonicOscillator, PyMetropolisAlgorithm, PyImportanceMetropolis, PySampler
 from matplotlib import pyplot as plt 
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
@@ -17,30 +17,37 @@ omega = 1
 no_of_alphas = 11
 alphas = np.linspace(0.3, 0.7, no_of_alphas)
 
+# !@$#!?...
+num_local_energies = 100
+stride_local_energies = num_samples // num_local_energies
+
 wavefunction_1 = PySimpleGaussian(num_particles, num_dimensions, num_parameters, mass, omega)
 wavefunction_2 = PySimpleGaussian(num_particles, num_dimensions, num_parameters, mass, omega)
 hamiltonian_1 = PyHarmonicOscillator(mass, omega)
 hamiltonian_2 = PyHarmonicOscillator(mass, omega)
 regular_metropolis_solver = PyMetropolisAlgorithm(num_particles)
 importance_metropolis_solver = PyImportanceMetropolis(num_particles)
+sampler_1 = PySampler(wavefunction_1, hamiltonian_1, regular_metropolis_solver, num_local_energies, stride_local_energies)
+sampler_2 = PySampler(wavefunction_2, hamiltonian_2, importance_metropolis_solver, num_local_energies, stride_local_energies)
 
 energies = np.zeros((2, no_of_alphas)) 
 
 for i in range(no_of_alphas):
 
-    wavefunction_1.redistribute()
-    wavefunction_2.redistribute()
     wavefunction_1.set_parameters(np.array([alphas[i]]))
     wavefunction_2.set_parameters(np.array([alphas[i]]))
 
-    regular_energy    = regular_metropolis_solver.run(wavefunction_1, hamiltonian_1, step_length, num_samples)
-    importance_energy = importance_metropolis_solver.run(wavefunction_2, hamiltonian_2, step_length, num_samples) 
+    sampler_1.sample(num_samples, step_length)
+    sampler_2.sample(num_samples, step_length)
 
-    regular_energy = regular_energy / num_samples
-    importance_energy = importance_energy / num_samples
+    regular_energy = sampler_1.get_energy() 
+    importance_energy = sampler_2.get_energy()
 
     energies[0, i] = regular_energy 
     energies[1, i] = importance_energy 
+
+    wavefunction_1.redistribute()
+    wavefunction_2.redistribute()
 
     print("alpha: {:.3f}, Reg. metr. energy: {:.3f}, Imp. samp. energy. {:.3f}.".format(alphas[i], regular_energy, importance_energy))
 
