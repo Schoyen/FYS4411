ecc243eed0e39615b0e5a5384a3bccdfb67a6814
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2018-03-03 15:12:20 +0100

    Add testing section of the gradients

--- project_1/vmc_hybrid/scripts/steepest_descent.py
+++ project_1/vmc_hybrid/scripts/steepest_descent.py
@@ -20,9 +20,10 @@ import numpy as np
 num_particles = 10
 num_dimensions = 1
 num_parameters = 1
-spread = 5.0
+spread = 1.0
 step_length = 0.05
-num_samples = 500000
+num_samples = int(1e6)
+num_thermalization_steps = int(1e6)
 
 mass = 1
 omega = 1
@@ -36,26 +37,40 @@ wavefunction = PySimpleGaussian(num_particles, num_dimensions, num_parameters, m
 solver = PyImportanceMetropolis(num_particles)
 sampler = PySampler(wavefunction, hamiltonian, solver, num_local_energies, stride_local_energies)
 
+alpha = 1.0
+wavefunction.set_parameters(np.array([alpha]))
+gradient = 0
+experiments = 100
+for i in range(1, experiments + 1):
+    sampler.sample(num_samples, step_length, num_thermalization_steps)
+    gradient += sampler.get_energy_gradient()
+    wavefunction.redistribute()
+    print ("Intermediate gradient: {0:f}".format(gradient/i))
+
+print ("Final gradient: {0:f}".format(gradient/experiments))
+__import__('sys').exit()
+
 alphas = np.linspace(0.1, 1.1, 11)
 
-'''
 for alpha in alphas:
     print("Alpha = ", alpha)
     wavefunction.set_parameters(np.array([alpha]))
     print("Start: ", sampler.get_energy_gradient())
     sampler.sample(num_samples, step_length)
     print("End: ", sampler.get_energy_gradient())
-'''
+    wavefunction.redistribute()
 
-alpha = 0.9 # start
+alpha = 0.5 # start
 
 MAX_ITER = 100
 iterations = 0
 iters_since_last_improvement = 0
-gamma = 0.3
+gamma = 1.0
 wavefunction.set_parameters(np.array([alpha]))
-sampler.sample(num_samples, step_length)
-gradient = sampler.get_energy_gradient() / num_particles
+sampler.sample(
+        num_samples, step_length,
+        num_thermalization_steps=num_thermalization_steps)
+gradient = sampler.get_energy_gradient()# / num_particles
 energy = sampler.get_energy()
 
 alphas = np.zeros(MAX_ITER)
@@ -70,8 +85,10 @@ while (iterations < MAX_ITER):
     energy_prev = energy
     energy = sampler.get_energy()
     gradient_prev = gradient
-    sampler.sample(num_samples, step_length)
-    gradient = sampler.get_energy_gradient() /num_particles
+    sampler.sample(
+            num_samples, step_length,
+            num_thermalization_steps=num_thermalization_steps)
+    gradient = sampler.get_energy_gradient()# /num_particles
 
     if (gradient*gradient > gradient_prev*gradient_prev):
         if (iters_since_last_improvement >= 10):
@@ -79,16 +96,15 @@ while (iterations < MAX_ITER):
             print("Stuck!")
         else:
             gamma = gamma * 0.9
-        
+
         iters_since_last_improvement += 1
     else:
         iters_since_last_improvement = 0
 
-    alphas[iterations] = alpha
-
     if alpha < 0:
         alpha = -alpha
 
+    alphas[iterations] = alpha
     iterations += 1
 
 plt.plot(alphas)
