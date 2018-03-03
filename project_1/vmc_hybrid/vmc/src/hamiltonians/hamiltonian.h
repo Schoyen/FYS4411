3a70567c4e37b0878062d41cd726d1a5cf3bc255
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2018-03-03 18:16:56 +0100

    Remove hard sphere radius in hamiltonian

--- project_1/vmc_hybrid/vmc/src/hamiltonians/hamiltonian.h
+++ project_1/vmc_hybrid/vmc/src/hamiltonians/hamiltonian.h
@@ -6,9 +6,6 @@
 
 class Hamiltonian
 {
-    protected:
-        double m_a = 0.0043;
-
     public:
         virtual double compute_local_energy(Wavefunction *wavefunction) = 0;
         virtual double compute_potential_energy(Wavefunction *wavefunction) = 0;
