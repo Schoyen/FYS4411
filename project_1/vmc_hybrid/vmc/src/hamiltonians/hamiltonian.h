970098770ee61cd3aae4bacd03a4158450a6c30a
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2018-03-03 17:38:04 +0100

    Add interaction function in Hamiltonian

--- project_1/vmc_hybrid/vmc/src/hamiltonians/hamiltonian.h
+++ project_1/vmc_hybrid/vmc/src/hamiltonians/hamiltonian.h
@@ -6,9 +6,14 @@
 
 class Hamiltonian
 {
+    protected:
+        double m_a = 0.0043;
+
     public:
         virtual double compute_local_energy(Wavefunction *wavefunction) = 0;
         virtual double compute_potential_energy(Wavefunction *wavefunction) = 0;
         virtual std::valarray<double> compute_local_energy_gradient(
                 Wavefunction *wavefunction) = 0;
+
+        double compute_interaction(Wavefunction *wavefunction);
 };
