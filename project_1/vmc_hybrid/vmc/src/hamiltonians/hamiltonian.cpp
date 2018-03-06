6a80548f09158a74051144a3b302d7965a125c97
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2018-03-06 15:39:58 +0100

    Move local energy calculation to super class
    	We let subclasses implement the kinetic energy and potential
    	energy functions.

--- project_1/vmc_hybrid/vmc/src/hamiltonians/hamiltonian.cpp
+++ project_1/vmc_hybrid/vmc/src/hamiltonians/hamiltonian.cpp
@@ -3,6 +3,18 @@
 #include "hamiltonian.h"
 #include "wavefunction.h"
 
+
+
+double Hamiltonian::compute_local_energy(Wavefunction *wavefunction)
+{
+    double kinetic_energy, potential_energy;
+
+    kinetic_energy = compute_kinetic_energy(wavefunction);
+    potential_energy = compute_potential_energy(wavefunction);
+
+    return kinetic_energy + potential_energy;
+}
+
 double Hamiltonian::compute_interaction(Wavefunction *wavefunction)
 {
     double a, distance;
