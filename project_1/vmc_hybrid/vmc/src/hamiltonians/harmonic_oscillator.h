4becfb7db4a4a895fea86454138268a1838e7b56
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2018-03-06 15:41:18 +0100

    Add kinetic energy calculation

--- project_1/vmc_hybrid/vmc/src/hamiltonians/harmonic_oscillator.h
+++ project_1/vmc_hybrid/vmc/src/hamiltonians/harmonic_oscillator.h
@@ -10,7 +10,7 @@ class HarmonicOscillator : public Hamiltonian
     public:
         using Hamiltonian::Hamiltonian;
 
-        double compute_local_energy(Wavefunction *wavefunction);
+        double compute_kinetic_energy(Wavefunction *wavefunction);
         double compute_potential_energy(Wavefunction *wavefunction);
         std::valarray<double> compute_local_energy_gradient(
                 Wavefunction *wavefunction);
