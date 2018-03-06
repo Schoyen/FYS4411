9397239284f9d780ac3619627810dba4e4c40681
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2018-03-06 14:46:55 +0100

    Add header of the elliptical hamiltonian

---
+++ project_1/vmc_hybrid/vmc/src/hamiltonians/elliptical_harmonic_oscillator.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "wavefunction.h"
+
+class EllipticalHarmonicOscillator
+{
+    private:
+        double m_lambda;
+
+    public:
+        EllipticalHarmonicOscillator(double lambda);
+
+        double compute_local_energy(Wavefunction *wavefunction);
+        double compute_potential_energy(Wavefunction *wavefunction);
+        std::valarray<double> compute_local_energy_gradient(
+                Wavefunction *wavefunction);
+}
