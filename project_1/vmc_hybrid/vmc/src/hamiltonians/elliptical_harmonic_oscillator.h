e4da4ebcb8edb8f10798bbba25689613eed4cf60
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2018-03-06 15:40:52 +0100

    Add overriding of kinetic energy

--- project_1/vmc_hybrid/vmc/src/hamiltonians/elliptical_harmonic_oscillator.h
+++ project_1/vmc_hybrid/vmc/src/hamiltonians/elliptical_harmonic_oscillator.h
@@ -4,6 +4,7 @@
 
 #include "hamiltonian.h"
 #include "wavefunction.h"
+#include "interacting_elliptical_gaussian.h"
 
 class EllipticalHarmonicOscillator : public Hamiltonian
 {
@@ -13,8 +14,6 @@ class EllipticalHarmonicOscillator : public Hamiltonian
     public:
         EllipticalHarmonicOscillator(double lambda);
 
-        double compute_local_energy(Wavefunction *wavefunction);
+        double compute_kinetic_energy(Wavefunction *wavefunction);
         double compute_potential_energy(Wavefunction *wavefunction);
-        std::valarray<double> compute_local_energy_gradient(
-                Wavefunction *wavefunction);
 };
