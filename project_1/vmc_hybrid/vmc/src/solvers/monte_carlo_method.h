713691ba77f8801b12612bd408a6467c99f21079
Mint-kontor <oyvindschoyen@gmail.com>
2018-03-12 15:15:00 +0100

    Add functions for getting random numbers
            This more resembles NumPy and avoids the need for creating a
            bunch of distributions in different functions.

--- project_1/vmc_hybrid/vmc/src/solvers/monte_carlo_method.h
+++ project_1/vmc_hybrid/vmc/src/solvers/monte_carlo_method.h
@@ -14,4 +14,28 @@ class MonteCarloMethod
         MonteCarloMethod(int seed);
 
         virtual bool step(Wavefunction *wavefunction, double step_length) = 0;
+
+        double next_uniform()
+        {
+            std::uniform_real_distribution<double> dist(0, 1);
+            return dist(m_engine);
+        }
+
+        double next_uniform(const double &min, const double &max)
+        {
+            std::uniform_real_distribution<double> dist(min, max);
+            return dist(m_engine);
+        }
+
+        int next_int(const int &min, const int &max)
+        {
+            std::uniform_int_distribution<int> dist(min, max);
+            return dist(m_engine);
+        }
+
+        int next_gaussian(const double &mean, const double &std)
+        {
+            std::normal_distribution<double> dist(mean, std);
+            return dist(m_engine);
+        }
 };
