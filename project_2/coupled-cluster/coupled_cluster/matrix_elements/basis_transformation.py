cff04e18ca9939e3b484a6aaa176768aa81972ca
Mint-kontor <oyvindschoyen@gmail.com>
2018-05-15 11:37:29 +0200

    Add two-body transformation

--- project_2/coupled-cluster/coupled_cluster/matrix_elements/basis_transformation.py
+++ project_2/coupled-cluster/coupled_cluster/matrix_elements/basis_transformation.py
@@ -7,3 +7,10 @@ def transform_one_body_elements(h, c):
     _h[np.abs(_h) < 1e-8] = 0
 
     return sparse.COO.from_numpy(_h)
+
+def transform_two_body_elements(u, c):
+    _u = np.einsum("pi, qj, kr, ls, ijkl -> pqrs", c.conj(), c.conj(), c, c, u)
+
+    _u[np.abs(_u) < 1e-8] = 0
+
+    return sparse.COO.from_numpy(_u)
