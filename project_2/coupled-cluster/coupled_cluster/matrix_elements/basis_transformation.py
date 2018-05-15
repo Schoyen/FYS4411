90413a553f3a39a8556db069dd60bbf55727641c
Mint-kontor <oyvindschoyen@gmail.com>
2018-05-15 11:22:04 +0200

    Start on conversion code

---
+++ project_2/coupled-cluster/coupled_cluster/matrix_elements/basis_transformation.py
@@ -0,0 +1,9 @@
+import numpy as np
+import sparse
+
+def transform_one_body_elements(h, c):
+    _h = np.einsum("ip, jq, ij -> pq", c.conj().T, c, h)
+
+    _h[np.abs(_h) < 1e-8] = 0
+
+    return sparse.COO.from_numpy(_h)
