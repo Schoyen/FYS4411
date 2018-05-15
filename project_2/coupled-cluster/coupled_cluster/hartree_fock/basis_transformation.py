b367d192ccea4d9871e621eb331e12c7e8346441
Mint-kontor <oyvindschoyen@gmail.com>
2018-05-15 15:48:16 +0200

    Fix ordering of indices

--- project_2/coupled-cluster/coupled_cluster/hartree_fock/basis_transformation.py
+++ project_2/coupled-cluster/coupled_cluster/hartree_fock/basis_transformation.py
@@ -2,7 +2,7 @@ import numpy as np
 import sparse
 
 def transform_one_body_elements(h, c):
-    _h = np.einsum("pi, jq, ij -> pq", c.conj(), c, h)
+    _h = np.einsum("ip, jq, ij -> pq", c.conj(), c, h)
 
     _h[np.abs(_h) < 1e-8] = 0
 
