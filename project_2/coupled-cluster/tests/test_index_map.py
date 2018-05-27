a6acc6c6675a78a3c44576fec40e2f3e2cf51644
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2018-05-27 23:25:50 +0200

    Add temporary break of test during development
    	Either the test data (coulomb_L_72.dat) is wrong or the
    	generation of the matrix elements from the inherited C++-code is
    	wrong.

--- project_2/coupled-cluster/tests/test_index_map.py
+++ project_2/coupled-cluster/tests/test_index_map.py
@@ -1,15 +1,22 @@
 import pytest
 
 from coupled_cluster.matrix_elements.index_map import (
-        get_index_p, get_indices_nm, generate_index_map
+        get_index_p, get_indices_nm, generate_index_map, IndexMap
 )
 
 def test_index_map(index_map):
-    num_shells = pytest.num_shells
+    num_shells = pytest.large_num_shells
     generate_index_map(num_shells)
+
+    print (num_shells)
+    print (IndexMap.p_list)
+    print (IndexMap.nm_dict)
+    print (IndexMap.shell_arr)
 
     for p in index_map:
         assert index_map[p] == get_indices_nm(p)
 
     for p, (n, m) in index_map.items():
         assert p == get_index_p(n, m)
+
+    assert False
