8eff2cffdfe1b5a7088fe28fd3d41f242e4c27aa
Mint-kontor <oyvindschoyen@gmail.com>
2018-05-28 18:07:38 +0200

    Remove temporary test code

--- project_2/coupled-cluster/tests/test_index_map.py
+++ project_2/coupled-cluster/tests/test_index_map.py
@@ -8,15 +8,8 @@ def test_index_map(index_map):
     num_shells = pytest.large_num_shells
     generate_index_map(num_shells)
 
-    print (num_shells)
-    print (IndexMap.p_list)
-    print (IndexMap.nm_dict)
-    print (IndexMap.shell_arr)
-
     for p in index_map:
         assert index_map[p] == get_indices_nm(p)
 
     for p, (n, m) in index_map.items():
         assert p == get_index_p(n, m)
-
-    assert False
