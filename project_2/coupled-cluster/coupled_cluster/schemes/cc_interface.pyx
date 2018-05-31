4b3fa70bc7dbc0fc3cb999edf847e2e0cb00ac23
Mint-kontor <oyvindschoyen@gmail.com>
2018-05-31 19:10:05 +0200

    Use C-libraries and remove array checks

--- project_2/coupled-cluster/coupled_cluster/schemes/cc_interface.pyx
+++ project_2/coupled-cluster/coupled_cluster/schemes/cc_interface.pyx
@@ -1,18 +1,37 @@
+import cython
+
 import numpy as np
 cimport numpy as np
 
-def _amplitude_scaling_two_body(np.ndarray[int, ndim=2, mode="c"] indices,
-        np.ndarray[double, ndim=1, mode="c"] data,
-        np.ndarray[double, ndim=2, mode="c"] h, int n, double tol=1e-10):
-    cdef int a, b, i, j, index
-    cdef double divisor
+from libc.math cimport fabs
 
-    for index in range(len(data)):
-        a, b, i, j = indices[index]
+@cython.wraparound(False)
+@cython.boundscheck(False)
+@cython.cdivision(True)
+def _amplitude_scaling_two_body(
+        np.ndarray[unsigned char, ndim=2] indices,
+        np.ndarray[double, ndim=1] data,
+        np.ndarray[double, ndim=2] h, int n, double tol=1e-10):
+    cdef int a, b, i, j, index, length
+    cdef double divisor
+    cdef np.ndarray[unsigned char, ndim=1] a_arr, b_arr, i_arr, j_arr
+
+    a_arr = indices[0]
+    b_arr = indices[1]
+    i_arr = indices[2]
+    j_arr = indices[3]
+
+    length = len(data)
+
+    for index in range(length):
+        a = a_arr[index]
+        b = b_arr[index]
+        i = i_arr[index]
+        j = j_arr[index]
 
         divisor = h[i, i] + h[j, j] - h[n + a, n + a] - h[n + b, n + b]
 
-        if abs(divisor) < tol:
+        if fabs(divisor) < tol:
             continue
 
         data[index] = data[index]/divisor
