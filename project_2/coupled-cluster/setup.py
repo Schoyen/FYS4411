8cddce218b7805173e00c4a672e41c59e0ef49ce
Mint-kontor <oyvindschoyen@gmail.com>
2018-05-31 18:23:49 +0200

    Build coupled cluster interface in setup

--- project_2/coupled-cluster/setup.py
+++ project_2/coupled-cluster/setup.py
@@ -13,18 +13,31 @@ if platform.system() == "Darwin":
     os.environ["CFLAGS"] += " -stdlib=libc++"
 
 base_path = ["coupled_cluster"]
-interface_path = base_path + ["matrix_elements"]
-source_path =  interface_path + ["src"]
 
-source_files = [
-    *glob.glob(os.path.join(*interface_path, "*.pyx")),
-    *glob.glob(os.path.join(*source_path, "*.cpp"))
-]
+coulomb_interface_path = base_path + ["matrix_elements"]
+coulomb_source_path =  coulomb_interface_path + ["src"]
 
-include_dirs = [
-    os.path.join(*source_path),
-    np.get_include()
-]
+cc_interface_path = base_path + ["schemes"]
+
+source_files = {
+    "coulomb_interface": [
+        *glob.glob(os.path.join(*coulomb_interface_path, "*.pyx")),
+        *glob.glob(os.path.join(*coulomb_source_path, "*.cpp"))
+    ],
+    "cc_interface": [
+        *glob.glob(os.path.join(*cc_interface_path, "*.pyx"))
+    ]
+}
+
+include_dirs = {
+    "coulomb_interface": [
+        os.path.join(*coulomb_source_path),
+        np.get_include()
+    ],
+    "cc_interface": [
+        np.get_include()
+    ]
+}
 
 libraries = []
 
@@ -35,9 +48,18 @@ undef_macros = []
 extensions = [
     Extension(
         name="coupled_cluster.matrix_elements.coulomb_interface",
-        sources=source_files,
+        sources=source_files["coulomb_interface"],
         language="c++",
-        include_dirs=include_dirs,
+        include_dirs=include_dirs["coulomb_interface"],
+        libraries=libraries,
+        define_macros=define_macros,
+        undef_macros=undef_macros
+    ),
+    Extension(
+        name="coupled_cluster.schemes.cc_interface",
+        sources=source_files["cc_interface"],
+        language="c",
+        include_dirs=include_dirs["cc_interface"],
         libraries=libraries,
         define_macros=define_macros,
         undef_macros=undef_macros
