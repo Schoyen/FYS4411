fefaa6119cbb04f4329166e1060fd0270d4a3c41
Mint-kontor <oyvindschoyen@gmail.com>
2018-06-06 00:50:04 +0200

    Add start of rewrite to numpy matmul
            Matmul and dot is vastly superior to einsum in terms of speed.
            Also, the two former supports automatic parallelization using
            parallel-BLAS.

--- project_2/coupled-cluster/coupled_cluster/schemes/ccd_optimized.py
+++ project_2/coupled-cluster/coupled_cluster/schemes/ccd_optimized.py
@@ -17,13 +17,37 @@ class CoupledClusterDoublesOptimized(CoupledClusterDoubles):
         super(CoupledClusterDoublesOptimized, self)._initialize(**kwargs)
 
         o, v = self.o, self.v
+        n, m = self.n, self.m
 
         # Allocate memory for the intermediates
         self.chi_abcd = np.zeros((self.m, self.m, self.m, self.m))
         self.chi_ad = np.zeros((self.m, self.m))
         self.chi_bmjc = np.zeros((self.m, self.n, self.n, self.m))
-        self.chi_bjcm = np.zeros((self.m, self.n, self.m, self.n))
         self.chi_nj = np.zeros((self.n, self.n))
+
+        self.chi_cmbj = np.zeros((m * n, m * n))
+
+        self.u_hhpp = self.u[o, o, v, v]\
+                .reshape(n * n, m * m)
+        self.u_hhpp_phh_p = self.u[o, o, v, v]\
+                .transpose(2, 0, 1, 3)\
+                .reshape(m * n * n, m)
+        self.u_hhpp_phph = self.u[o, o, v, v]\
+                .transpose(3, 1, 2, 0)\
+                .reshape(m * n, m * n)
+        self.u_hhpp_h_pph = self.u[o, o, v, v]\
+                .transpose(1, 2, 3, 0)\
+                .reshape(n, m * m * n)
+
+        self.t_phph = self.t\
+                .transpose(0, 2, 1, 3)\
+                .reshape(m * n, m * n)
+        self.t_pph_h = self.t\
+                .transpose(0, 1, 3, 2)\
+                .reshape(m * m * n, n)
+        self.t_p_phh = self.t\
+                .transpose(1, 0, 2, 3)\
+                .reshape(m, m * n * n)
 
         self._t_ijab = np.zeros((self.n, self.n, self.m, self.m))
         self._t_iabj = np.zeros((self.n, self.m, self.m, self.n))
@@ -52,20 +76,37 @@ class CoupledClusterDoublesOptimized(CoupledClusterDoubles):
         #    self._compute_two_body_amplitude()
 
         print ("Starting parallel timing...")
+        t0 = time.time()
         self._compute_intermediates_parallel()
         self._compute_one_body_amplitude_parallel()
         self._compute_two_body_amplitude_parallel()
+        t1 = time.time()
+        print ("Parallel total time: {0} sec".format(t1 - t0))
 
-        print ("\nStarting serial timing...")
+
         self._t.fill(0)
+        print ("\nStarting serial timing...")
+        t0 = time.time()
         self._compute_intermediates()
         self._compute_one_body_amplitude()
         self._compute_two_body_amplitude()
+        t1 = time.time()
+        print ("Serial total time: {0} sec".format(t1 - t0))
 
         print ("\n")
 
         amplitude_scaling_two_body(self._t, self.f, self.m, self.n)
         self.t = np.add((1 - theta) * self._t, theta * self.t, out=self.t)
+
+        self.t_phph[:] = self.t\
+                .transpose(0, 2, 1, 3)\
+                .reshape(self.m * self.n, self.m * self.n)
+        self.t_pph_h[:] = self.t\
+                .transpose(0, 1, 3, 2)\
+                .reshape(self.m * self.m * self.n, self.n)
+        self.t_p_phh[:] = self.t\
+                .transpose(1, 0, 2, 3)\
+                .reshape(self.m, self.m * self.n * self.n)
 
         self._t_ijab[:] = self.t.transpose(2, 3, 0, 1)
         self._t_iabj[:] = self.t.transpose(2, 0, 1, 3)
@@ -137,6 +178,11 @@ class CoupledClusterDoublesOptimized(CoupledClusterDoubles):
                 self.u[v, v, v, v], self.n, self.m)
         t1 = time.time()
         print ("parallel chi_abcd: {0} sec".format(t1 - t0))
+        #chi_abcd = 0.25 * np.einsum(
+        #        "abmn, mncd -> abcd", self.t, self.u[o, o, v, v])
+        #chi_abcd += 0.5 * self.u[v, v, v, v]
+        #np.testing.assert_allclose(self.chi_abcd, chi_abcd, rtol=1e-8,
+        #        atol=1e-8)
 
         t0 = time.time()
         compute_chi_ad(self.chi_ad, self.t, self.u, self.n, self.m)
@@ -147,13 +193,6 @@ class CoupledClusterDoublesOptimized(CoupledClusterDoubles):
         compute_chi_bmjc(
                 self.chi_bmjc, self._t_iabj, self._u_hpph,
                 self.u[v, o, o, v], self.n, self.m)
-        t1 = time.time()
-        print ("parallel chi_bmjc: {0} sec".format(t1 - t0))
-
-        t0 = time.time()
-        compute_chi_bjcm(
-                self.chi_bjcm, self._t_aibj, self._u_phph,
-                self._u_phph_2, self.n, self.m)
         t1 = time.time()
         print ("parallel chi_bmjc: {0} sec".format(t1 - t0))
 
@@ -185,16 +224,44 @@ class CoupledClusterDoublesOptimized(CoupledClusterDoubles):
     @profile
     def _compute_two_body_amplitude(self):
         o, v = self.o, self.v
+        n, m = self.n, self.m
 
         self._t += self.u[v, v, o, o]
+
+        t0 = time.time()
+        np.matmul(
+                self.chi_abcd.reshape(m * m, m * m),
+                self.t.reshape(m * m, n * n),
+                out=self.term.reshape(m * m, n * n)
+        )
+        t1 = time.time()
+        print ("matmul chi_abcd contraction: {0} sec".format(t1 - t0))
+        self._t += self.term
+        #term = np.einsum(
+        #        "cdij, abcd -> abij", self.t, self.chi_abcd)
+        #np.testing.assert_allclose(self.term, term, atol=1e-8, rtol=1e-8)
 
         t0 = time.time()
         self.term = np.einsum(
                 "cdij, abcd -> abij", self.t, self.chi_abcd,
                 out=self.term, optimize="optimal")
         t1 = time.time()
-        print ("chi_abcd contraction: {0} sec".format(t1 - t0))
+        print ("einsum chi_abcd contraction: {0} sec".format(t1 - t0))
+
+        t0 = time.time()
+        np.matmul(
+                self.t.reshape(m * m * n, n),
+                self.chi_nj,
+                out=self.term.reshape(m * m * n, n)
+        )
+        self.term -= self.term.swapaxes(2, 3)
+        t1 = time.time()
+        print ("matmul chi_nj contraction: {0} sec".format(t1 - t0))
         self._t += self.term
+        #term = np.einsum(
+        #        "abin, nj -> abij", self.t, self.chi_nj)
+        #term -= term.swapaxes(2, 3)
+        #np.testing.assert_allclose(self.term, term, atol=1e-8, rtol=1e-8)
 
         t0 = time.time()
         self.term = np.einsum(
@@ -202,8 +269,23 @@ class CoupledClusterDoublesOptimized(CoupledClusterDoubles):
                 out=self.term, optimize="optimal")
         self.term -= self.term.swapaxes(2, 3)
         t1 = time.time()
-        print ("chi_nj contraction: {0} sec".format(t1 - t0))
+        print ("einsum chi_nj contraction: {0} sec".format(t1 - t0))
+
+        t0 = time.time()
+        np.matmul(
+                self.chi_ad,
+                self.t_p_phh,
+                out=self.term.reshape(m, m * n * n)
+        )
+        self.term *= -1
+        self.term -= self.term.swapaxes(0, 1)
+        t1 = time.time()
+        print ("matmul chi_ad contraction: {0} sec".format(t1 - t0))
         self._t += self.term
+        #term = - np.einsum(
+        #        "bdij, ad -> abij", self.t, self.chi_ad)
+        #term -= term.swapaxes(0, 1)
+        #np.testing.assert_allclose(self.term, term, atol=1e-8, rtol=1e-8)
 
         t0 = time.time()
         self.term = - np.einsum(
@@ -211,8 +293,28 @@ class CoupledClusterDoublesOptimized(CoupledClusterDoubles):
                 out=self.term, optimize="optimal")
         self.term -= self.term.swapaxes(0, 1)
         t1 = time.time()
-        print ("chi_ad contraction: {0} sec".format(t1 - t0))
+        print ("einsum chi_ad contraction: {0} sec".format(t1 - t0))
+
+        t0 = time.time()
+        self.term[:] = np.matmul(
+                self.t_phph,
+                self.chi_cmbj
+        ).reshape(m, n, m, n).transpose(0, 2, 1, 3)
+        #np.testing.assert_allclose(
+        #        np.einsum("acim, bmjc -> abij", self.t, self.chi_bmjc),
+        #        self.term, atol=1e-8, rtol=1e-8)
+        self.term -= self.term.swapaxes(0, 1)
+        self.term -= self.term.swapaxes(2, 3)
+        t1 = time.time()
+        print ("matmul chi_cmbj contraction: {0} sec".format(t1 - t0))
         self._t += self.term
+        term = np.einsum(
+                "aicm, cmbj -> abij", self.t.transpose(0, 2, 1, 3),
+                self._chi_cmbj)
+        term -= term.swapaxes(0, 1)
+        term -= term.swapaxes(2, 3)
+        np.testing.assert_allclose(self.term, term, atol=1e-8, rtol=1e-8)
+
 
         t0 = time.time()
         self.term = np.einsum(
@@ -221,8 +323,7 @@ class CoupledClusterDoublesOptimized(CoupledClusterDoubles):
         self.term -= self.term.swapaxes(0, 1)
         self.term -= self.term.swapaxes(2, 3)
         t1 = time.time()
-        print ("chi_bmjc contraction: {0} sec".format(t1 - t0))
-        self._t += self.term
+        print ("einsum chi_bmjc contraction: {0} sec".format(t1 - t0))
 
         t0 = time.time()
         self.term = 0.5 * np.einsum(
@@ -235,6 +336,23 @@ class CoupledClusterDoublesOptimized(CoupledClusterDoubles):
     @profile
     def _compute_intermediates(self):
         o, v = self.o, self.v
+        n, m = self.n, self.m
+
+        t0 = time.time()
+        np.matmul(
+                self.t.reshape(m * m, n * n),
+                self.u_hhpp,
+                out=self.chi_abcd.reshape(m * m, m * m)
+        )
+        self.chi_abcd *= 0.25
+        self.chi_abcd += 0.5 * self.u[v, v, v, v]
+        t1 = time.time()
+        print ("matmul chi_abcd: {0} sec".format(t1 - t0))
+        #chi_abcd = 0.25 * np.einsum(
+        #        "abmn, mncd -> abcd", self.t, self.u[o, o, v, v])
+        #chi_abcd += 0.5 * self.u[v, v, v, v]
+        #np.testing.assert_allclose(
+        #        self.chi_abcd, chi_abcd, rtol=1e-8, atol=1e-8)
 
         t0 = time.time()
         self.chi_abcd = 0.25 * np.einsum(
@@ -242,14 +360,48 @@ class CoupledClusterDoublesOptimized(CoupledClusterDoubles):
                 out=self.chi_abcd, optimize="optimal")
         self.chi_abcd += 0.5 * self.u[v, v, v, v]
         t1 = time.time()
-        print ("chi_abcd: {0} sec".format(t1 - t0))
+        print ("einsum chi_abcd: {0} sec".format(t1 - t0))
+
+        t0 = time.time()
+        np.matmul(
+                self.t.reshape(m, m * n * n),
+                self.u_hhpp_phh_p,
+                out=self.chi_ad
+        )
+        self.chi_ad *= 0.5
+        t1 = time.time()
+        print ("matmul chi_ad: {0} sec".format(t1 - t0))
 
         t0 = time.time()
         self.chi_ad = 0.5 * np.einsum(
                 "acnm, nmcd -> ad", self.t, self.u[o, o, v, v],
                 out=self.chi_ad, optimize="optimal")
         t1 = time.time()
-        print ("chi_ad: {0} sec".format(t1 - t0))
+        print ("einsum chi_ad: {0} sec".format(t1 - t0))
+
+        t0 = time.time()
+        np.matmul(
+                self.t_phph,
+                self.u_hhpp_phph,
+                out=self.chi_cmbj
+        )
+        self.chi_cmbj *= 0.5
+        self.chi_cmbj += self.u_hhpp_phph
+        #np.testing.assert_allclose(
+        #        (0.5 * np.einsum(
+        #            "bdjn, mncd -> bjcm", self.t, self.u[o, o, v, v]) \
+        #                    + self.u[v, o, v, o]).reshape(m * n, m * n),
+        #        self.chi_cmbj,
+        #        atol=1e-8, rtol=1e-8)
+        self.chi_cmbj = (
+                0.5 * np.einsum(
+                    "bdjn, mncd -> bjcm", self.t, self.u[o, o, v, v]) \
+                            + self.u[v, o, v, o]
+        ).reshape(m * n, m * n)
+
+        self.chi_cmbj[:] = self.chi_cmbj.transpose(1, 0)
+        t1 = time.time()
+        print ("matmul chi_cmbj: {0} sec".format(t1 - t0))
 
         t0 = time.time()
         self.chi_bmjc = 0.5 * np.einsum(
@@ -257,11 +409,28 @@ class CoupledClusterDoublesOptimized(CoupledClusterDoubles):
                 out=self.chi_bmjc, optimize="optimal")
         self.chi_bmjc += self.u[v, o, o, v]
         t1 = time.time()
-        print ("chi_bmjc: {0} sec".format(t1 - t0))
+        print ("einsum chi_bmjc: {0} sec".format(t1 - t0))
+        np.testing.assert_allclose(
+                self.chi_cmbj,
+                self.chi_bmjc.transpose(3, 1, 0, 2).reshape(m * n, m * n),
+                atol=1e-8, rtol=1e-8)
+
+        t0 = time.time()
+        np.matmul(
+                self.u_hhpp_h_pph,
+                self.t_pph_h,
+                out=self.chi_nj
+        )
+        self.chi_nj *= 0.5
+        t1 = time.time()
+        print ("matmul chi_nj: {0} sec".format(t1 - t0))
+        chi_nj = 0.5 * np.einsum(
+                "cdjm, mncd -> nj", self.t, self.u[o, o, v, v])
+        np.testing.assert_allclose(self.chi_nj, chi_nj, atol=1e-8, rtol=1e-8)
 
         t0 = time.time()
         self.chi_nj = 0.5 * np.einsum(
                 "cdjm, mncd -> nj", self.t, self.u[o, o, v, v],
                 out=self.chi_nj, optimize="optimal")
         t1 = time.time()
-        print ("chi_nj: {0} sec".format(t1 - t0))
+        print ("einsum chi_nj: {0} sec".format(t1 - t0))
